2bb28cc658f621cf6d0f74e9930bada4b7d57cfe
darkroom51 <darkroom@wp.pl>
2017-12-07 12:08:15 +0100

    first wld_JS

---
+++ wld_JS1/js/day2.js
@@ -0,0 +1,32 @@
+/*var nullVar = null
+var undefinedVar = undefined
+var numberVar = 1
+var boolVar = true
+var stringVar = "ala"
+var arrayVar = [1,2,3]
+var objVar = {a: 1, b: 2}
+
+var typesArray = [
+    typeof nullVar,
+    typeof undefinedVar,
+    typeof numberVar,
+    typeof boolVar,
+    typeof stringVar,
+    typeof arrayVar,
+    typeof objVar
+]
+
+
+console.log(
+    typeof nullVar,
+    typeof undefinedVar,
+    typeof numberVar,
+    typeof boolVar,
+    typeof stringVar,
+    typeof arrayVar,
+    typeof objVar
+)
+
+console.table(typesArray)*/
+
+console.log('hello world');
