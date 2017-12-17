1614c2aa518964026a0652471f9c5e6d620429d3
Waldek Fryc <waldemar.fryc@gmail.com>
2017-12-17 11:54:25 +0100

    game 0.2

--- wld_JS1/axis/js/project.js
+++ wld_JS1/axis/js/project.js
@@ -1,77 +1,132 @@
+var env = {
+    'minPosX': 0,
+    'maxPosX': 100,
+    'minPosY': 0,
+    'maxPosY': 95,
+    'viewPortHpx': $('.game-board').height(),
+    'viewPortWpx': $('.game-board').width()
+}
+
+function calcProc(x) {
+    return Math.floor(x * 100 / env.viewPortWpx)
+}
+
+function createPad() {
+    var pad = {
+        x: 40,
+        y: 90,
+        width: 20,
+        init: function () {
+            var padd = $('<div class="pad" style="top: ' + this.y + '%;left: ' + this.x + '%;"></div>');
+            $('.game-board').append(padd);
+        },
+        movePad: function (z) {
+            this.x = z;
+            $('.pad').css('left', (this.x) + '%');
+            $('.pad').html('this.x: ' + this.x); //----test
+            //console.log(this.x); //--------------------wld_CL
+        },
+    }
+    return pad
+}
+
+var basket = createPad();
+basket.init();
+
+$('.game-board').mousemove(function (e) {
+    //console.log('page: ', e.pageX); //--------------------wld_CL
+    var newX = Math.floor(calcProc(e.pageX) - basket.width / 2 - calcProc($('.game-board').offset().left));
+    //console.log(newX); //--------------------wld_CL
+    if ((newX + basket.width < env.maxPosX) && (newX > env.minPosX)) {
+        basket.x = newX;
+    } else if (newX + basket.width >= env.maxPosX) {
+        basket.x = env.maxPosX - basket.width;
+    } else {
+        basket.x = env.minPosX;
+    }
+    //basket.x = newX;
+    basket.movePad(basket.x);
+});
+
+
 function createPoint(id) {
     var point = {
-        x: Math.round(Math.random()*100),
-        y: 10,
-        color: 'green',
         id: id,
-        speed: 5,
+        x: Math.round(Math.random() * 100),
+        y: 0,
+        type: 0,
+        speed: 1,
         init: function () {
-            var bullet = $('<div id="' + id + '" class="bullet" style="top: ' + this.y + '%;left: ' + this.x + '%; background-color: ' + this.color + '">'+id+'</div>');
-            $('.content').append(bullet);
-        },
-        moveRight: function () {
-            this.x = this.x + 1;
-            $('#' + id).css('left', (this.x) + '%');
+            var bullet = $('<div id="' + id + '" class="bullet" style="top: ' + this.y + '%; left: ' + this.x + '%; background-color: ' + (this.type == 0 ? 'green' : 'red') + '"></div>');
+            $('.game-board').append(bullet);
         },
         moveDown: function () {
-
-                this.y = this.y + this.speed;
-                $('#' + id).css('top', (this.y) + '%');
-                return this.y
-
+            this.y = this.y + this.speed;
+            $('#' + id).css('top', (this.y) + '%');
+            $('#' + id).html('#' + this.id + '<br>y: ' + this.y); //-------wld_test
+            //console.log('point: ' ,this.y); //--------------------wld_CL
+        },
+        removePoint: function () {
+            $('#' + id).remove();
         }
     };
-
     return point;
 }
 
-points = [];
+var points = [];
+
 var point = createPoint(1);
+point.type = (Math.round(Math.random() * 10)) < 5 ? 1 : 0;
 point.init();
 points.push(point);
 
 setInterval(function () {
-    point.moveDown();
+    if (point.y < env.maxPosY) {
+        point.moveDown();
+    } else {
+        point.removePoint();
+    }
 }, 100);
 
 
+var point2 = createPoint(2);
+point2.type = (Math.round(Math.random() * 10)) < 5 ? 1 : 0;
+point2.speed = 2;
+point2.init();
+points.push(point2);
 
+setInterval(function () {
+    if (point2.y < env.maxPosY) {
+        point2.moveDown();
+    } else {
+        point2.removePoint();
+    }
+}, 100);
+
+
+/*
+for (var i=0; i<4; i++){
+    var point = createPoint(i);
+    point.type = (Math.round(Math.random()*10))<5 ? 1 : 0;
+    point.init();
+    points.push(point);
+}
+
+var fLiveOfPoints = function (arr) {
+
+    for (var n=0; n<arr.lenght; n++) {
+        if (arr[i].y < env.maxPosY) {
+            arr[i].moveDown();
+        } else {
+            arr[i].removePoint();
+            arr.pop();
+        }
+    } // end of for
+}
+
+
+setInterval(fLiveOfPoints(points), 100);
+*/
 
 
 //------------------------------------
-
-var point2 = createPoint(2);
-point2.color = 'red';
-point2.speed = 10;
-point2.init();
-points.push(point2);
-setInterval(function () {
-    point2.moveDown();
-}, 100);
-
-var point3 = createPoint(3);
-point3.color = 'green';
-point3.speed = 30;
-point3.init();
-points.push(point3);
-setInterval(function () {
-    point3.moveDown();
-}, 100);
-
-var point4 = createPoint(4);
-point4.color = 'red';
-point4.speed = 30;
-point4.init();
-points.push(point4);
-setInterval(function () {
-    point4.moveDown();
-}, 100);
-
-var point5 = createPoint(5);
-point5.color = 'green';
-point5.speed = 10;
-point5.init();
-points.push(point5);
-setInterval(function () {
-    point5.moveDown();
-}, 100);
